9e875e038895751694fe32cca4269a089d7d50e1
eimbachi <eimbachi@emeal.nttdata.com>
2025-02-24 10:07:24 -0500

    Update PublishLoggerTest.java
    100% methods, 88% lines covered

--- src/test/java/com/daicode/consumer_api/util/PublishLoggerTest.java
+++ src/test/java/com/daicode/consumer_api/util/PublishLoggerTest.java
@@ -2,6 +2,7 @@ package com.daicode.consumer_api.util;
 
 import com.daicode.consumer_api.dto.SubjectLoggerDTO;
 import com.daicode.consumer_api.exception.PublishLoggerException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.nats.client.Connection;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,13 +22,15 @@ import static org.mockito.Mockito.*;
 class PublishLoggerTest {
 
     @Mock
-    private ObjectMapper objectMapper;
+    private ObjectMapper mockObjectMapper;
 
     @Mock
-    private Connection natsConnection;
+    private Connection mockNatsConnection;
 
     @InjectMocks
     private PublishLogger publishLogger;
+    @Mock
+    private SubjectLoggerDTO mockSubjectLoggerDTO;
 
     @BeforeEach
     public void setUp() {
@@ -36,15 +39,38 @@ class PublishLoggerTest {
     }
 
     @Test
-    void testPublishSuccess() throws IOException, NoSuchAlgorithmException {
-        SubjectLoggerDTO dto = mock(SubjectLoggerDTO.class);
-        when(dto.getTransactionId()).thenReturn(UUID.randomUUID());
-        when(dto.generateHash()).thenReturn("someHash");
-        when(objectMapper.writeValueAsString(any())).thenReturn("jsonString");
+    void testPublishSuccess_request_response_stringJson() throws IOException, NoSuchAlgorithmException {
+        when(mockSubjectLoggerDTO.getTransactionId()).thenReturn(UUID.randomUUID());
+        when(mockSubjectLoggerDTO.generateHash()).thenReturn("someHash");
+        when(mockObjectMapper.writeValueAsString(any())).thenReturn("jsonString");
 
-        publishLogger.publish(dto, "{}", "{}");
+        publishLogger.publish(mockSubjectLoggerDTO, "{\"test\":\"test-response\"}", "{\"test\":\"test-response\"}");
 
-        verify(natsConnection, times(1)).publish(eq("test-subject"), any(byte[].class));
+        verify(mockNatsConnection, times(1)).publish(eq("test-subject"), any(byte[].class));
+    }
+
+    @Test
+    void testPublishSuccess_request_response_rawTextKey() throws IOException, NoSuchAlgorithmException {
+        when(mockSubjectLoggerDTO.getTransactionId()).thenReturn(UUID.randomUUID());
+        when(mockSubjectLoggerDTO.generateHash()).thenReturn("someHash");
+        when(mockObjectMapper.readTree(anyString())).thenThrow(JsonProcessingException.class);
+        when(mockObjectMapper.writeValueAsString(any())).thenReturn("textPlain");
+
+        publishLogger.publish(mockSubjectLoggerDTO, "rawTextKey", "rawTextKey");
+
+        verify(mockNatsConnection, times(1)).publish(eq("test-subject"), any(byte[].class));
+    }
+
+    @Test
+    void testPublishSuccess_request_response_isDTO() throws IOException, NoSuchAlgorithmException {
+        Object someDTO = mock(Object.class);
+        when(mockSubjectLoggerDTO.getTransactionId()).thenReturn(UUID.randomUUID());
+        when(mockSubjectLoggerDTO.generateHash()).thenReturn("someHash");
+        when(mockObjectMapper.writeValueAsString(any())).thenReturn("{\"string\":\"json\"}");
+
+        publishLogger.publish(mockSubjectLoggerDTO, someDTO, someDTO);
+
+        verify(mockNatsConnection, times(1)).publish(eq("test-subject"), any(byte[].class));
     }
 
     @Test
@@ -53,8 +79,23 @@ class PublishLoggerTest {
         when(dto.getTransactionId()).thenReturn(UUID.randomUUID());
         when(dto.generateHash()).thenThrow(NoSuchAlgorithmException.class);
 
-        assertThrows(PublishLoggerException.class, () -> {
-            publishLogger.publish(dto, "{}", "{}");
-        });
+        assertThrows(PublishLoggerException.class, () -> publishLogger.publish(dto, "{}", "{}"));
     }
+
+    @Test
+    void testPublish_ThrowsPublishLoggerExceptionOnJsonProcessingError() throws Exception {
+        String jsonString = "{\"key\":\"value\"}";
+
+        when(mockObjectMapper.writeValueAsString(any(SubjectLoggerDTO.class))).thenThrow(new JsonProcessingException("Error") {
+        });
+
+        assertThrows(PublishLoggerException.class, () -> publishLogger.publish(mock(SubjectLoggerDTO.class), jsonString, "response"));
+    }
+    /*@Test
+    void testPublish_JsonProcessingException() throws IOException {
+        when(mockSubjectLoggerDTO.getTransactionId()).thenReturn(UUID.randomUUID());
+        when(mockObjectMapper.readValue(anyString(), eq(TypeReference.class))).thenThrow(JsonProcessingException.class);
+
+        assertThrows(PublishLoggerException.class, () -> publishLogger.publish(mockSubjectLoggerDTO, "{}", "{}"));
+    }*/
 }
